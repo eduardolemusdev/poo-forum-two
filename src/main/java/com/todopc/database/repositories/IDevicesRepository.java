db0dd7d749918b40ead8fd822b771a11c44f537d
Eduardo Lemus <eduardolemus3098@gmail.com>
2024-09-21 22:04:18 -0600

    extends agregado tambien al generico desde la interfaz para solo extender del dispositivo

--- src/main/java/com/todopc/database/repositories/IDevicesRepository.java
+++ src/main/java/com/todopc/database/repositories/IDevicesRepository.java
@@ -1,8 +1,10 @@
 package com.todopc.database.repositories;
 
+import com.todopc.database.models.Device;
+
 import java.util.List;
 
-public interface IDevicesRepository<T> {
+public interface IDevicesRepository<T extends Device> {
     T saveDevice(T device);
     List<T> getDevices();
 }
